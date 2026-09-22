c7a95b45130acb221c47acafde84cee3d40febea
nneyamot <mohammadnneyamot@gmail.com>
2026-09-22 07:15:54 +0600

    sccond session

---
+++ scripts/PR_session_3.R
@@ -0,0 +1,33 @@
+
+a=3;
+if(a%%2==0)
+  print("yes");
+
+for(i in 2:5){
+  print(i)
+}
+
+#crate vector 
+
+v=c(3,4,3)
+print(v)
+print(v[2:3])
+
+seq(s,end , increment)
+seq(3,8,2)
+
+#factor  (levels group)
+gender = factor(c("male","female" , "female", "male"))
+print(gender)
+
+#matrix
+matrix(1:3)
+matrix(1:4, nrow=2)
+matrix(1:4, nrow=2 , ncol=2)
+matrix(1:4, nrow=2 , ncol=2 , byrow=TRUE)
+mat<- matrix(1:4, nrow=2 , ncol=2 , byrow=TRUE)
+dim(mat)
+
+
+
+
